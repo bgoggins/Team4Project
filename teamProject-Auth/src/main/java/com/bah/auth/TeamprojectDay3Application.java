d990b3125280159afb6d59bd7d10d28044d24901
Sean Bates <sean_b@live.com>
2023-02-22 21:45:21 +0000

    Day 3 Sec Authorization

---
+++ teamProject-Auth/src/main/java/com/bah/auth/TeamprojectDay3Application.java
@@ -0,0 +1,13 @@
+package com.bah.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamprojectDay3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamprojectDay3Application.class, args);
+	}
+
+}
